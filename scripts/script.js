8f528c8fefc0259574dc9df510cecbf09979ad1b
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-08-10 16:45:46 +0300

    initial commit - owl carousel

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+    $("#my_carousel").owlCarousel({
+      autoplay: true,
+      autoplayHoverPause: true,
+      items: 3,
+      nav:true,
+      loop: true
+    });
+    
+  });
