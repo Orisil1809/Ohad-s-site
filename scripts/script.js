b5b166d2d0374d5566197c8c03e2f87f98e33c35
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-08-10 23:59:48 +0300

    Add slick carousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,53 @@
-$(document).ready(function() {
- 
-    $("#my_carousel").owlCarousel({
-      autoplay: true,
-      autoplayHoverPause: true,
-      items: 3,
-      nav:true,
-      loop: true
-    });
-    
+$(document).ready(function(){
+  $('.slick').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots:true,
+  speed: 350,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  loop: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 848,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      }
+    }
+  ]
   });
+});
+
+
+$('#my_burger').click(function(){
+    
+});
