a8fab02ccbff9ac057e334f41552c37ac0cbe409
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-08-12 16:27:43 +0300

    Add background and our story animation

--- scripts/script.js
+++ scripts/script.js
@@ -55,3 +55,42 @@ $(document).ready(function(){
 // $('.navbar-toggler-icon').click(function(){
 //     $(this)
 // });
+
+$(window).scroll( function(){
+    if(window)
+  /* Check the location of each desired element */
+  $('#our_story').each( function(i){
+
+      // var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
+      const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
+      const bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > middle_of_object ){
+          console.log("asdasd");
+          
+          // $(this).animate({'opacity':'1'} ,'slow');
+          $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" }); //Linear or Swing?? Speed of animation? What on mobile?
+
+
+      }
+
+  }); 
+
+});
+
+// window.onload =  function(){
+//   console.log("PRESSED!!!");
+//   $('#our_story').each(function(i){
+//     $(this).animate({ width: 'show'});
+//   });
+// };
+
+$(".my_nav").click(function () { 
+    console.log("PRESSED!!!");
+    $('#our_story').each(function(i){
+      $(this).animate({ width: 'show'}, 'slow');
+    });
+
+  });
