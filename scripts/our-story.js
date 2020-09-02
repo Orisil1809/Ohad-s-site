0b08f6774ce20e678c9217e940c26a588c71e547
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-02 19:58:41 +0300

    Added small animation to second_row on our-story page

--- scripts/our-story.js
+++ scripts/our-story.js
@@ -192,4 +192,22 @@ $(document).ready(function(){
   //Commented 29.08
   
 
+  $(window).scroll( function(){
+
+    $('#second_row').each( function(i){
   
+      const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
+      const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
+      const bottom_of_window = $(window).scrollTop() + $(window).height();
+  
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > top_of_object ){          
+         
+          // $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" }); //Linear or Swing?? Speed of animation? What on mobile?
+  
+          $(this).addClass('anim');
+  
+      }
+  
+    });
+});
