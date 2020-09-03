0b4ed2d9fc962b9db57851d16e86f94809905578
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-04 00:21:15 +0300

    Add new navbar to menu page

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,43 @@
-
-$('.nav_menu').click(function(){
-    console.log("ORIIII");
-    $('#main_navbar').each(function(){
-        // if($(this).hasClass('colored')){
-        //     $(this).css("background-color", "transparent");
-        // }
-        // else{
-        //     $(this).animate({backgroundColor: '#FF0000'}, slow);
-
-        //     // $(this).css("background-color", "yellow");
-        // }
-
-        $(this).toggleClass('colored');
-        $('#my_brand').toggleClass('coffee_disappear');
-        //Change hover color on nav links
-        //Display none the #my_brand
+$('.navbar-toggler').click(function(){
+    $("#main_navbar").toggleClass('colored');
+    $('.colored #navbarCollapse').on("click", "li" , function(){
+      $('#navbarCollapse').collapse('hide');
+      $('.navbar-toggler').removeClass('opened');
+      $('.navbar-toggler').addClass('collapsed');
+      $('#main_navbar').removeClass('colored');
+  
+  
     });
-    
-
-});
+  
+  });
+  
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $("#main_navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() + 20 > 80);
+      $('.scrolled #navbarCollapse').on("click", "li" , function(){
+        console.log("ORI22");
+        $('#navbarCollapse').collapse('hide');
+        $('.navbar-toggler').removeClass('opened');
+        $('.navbar-toggler').addClass('collapsed');
+        $('#main_navbar').removeClass('colored');
+  
+  
+      });
+    });
+  });
+  
+  $('#navbarCollapse').on('hide.bs.collapse', function () {
+    // $('.my_nav').css({ 'opacity' : 0 });
+    $('.my_nav').animate({'opacity':'0'} ,'fast');
+  
+  });
+  
+  $('#navbarCollapse').on('show.bs.collapse', function () {
+    $('.my_nav').css({ 'opacity' : 1 });
+  
+  });
+  
+  function delay (URL) {
+    setTimeout( function() { window.location = URL }, 500 );
+  }
