954cd66ce1835d8071774b74e792668f8ac6777a
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-20 20:21:22 +0300

    try make animation work

--- scripts/second-hand.js
+++ scripts/second-hand.js
@@ -24,29 +24,8 @@ function delay (URL) {
   setTimeout( function() { window.location = URL }, 500 );
 }
 
-
-// $(window).scroll( function(){
-
-// $('.card').each( function(i){
-
-//     const top_of_object = $(this).offset().top;
-//     const middle_of_object = $(this).offset().top + $(this).outerHeight()/3;
-//     const bottom_of_window = $(window).scrollTop() + $(window).height();
-
-//     if( bottom_of_window > middle_of_object ){    
-
-//         $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" });
-
-
-//     }
-
-// }); 
-
-// });
-
 $(document).ready(function ($) {
 
-    // tl.fromTo(".card", { opacity: 0 }, { opacity: 1, delay: 0.5, duration: 1, stagger: 0.5});
     tl.fromTo(".card", { y: "100%" }, { y: "0%", delay: 0.2, duration: 1, stagger: 0.5});
 
   var url = window.location.href;
@@ -58,9 +37,6 @@ $(document).ready(function ($) {
           $(this).closest("li").addClass("active");
       }
   });
-    // $('.card-columns').animate({'width':'show'} , { duration: 1000, easing: "linear" }); //Linear or Swing?? Speed of animation? What on mobile?
-    // $('#second-card').animate({'width':'show'} , { duration: 1000, queue: true, easing: "linear" }); //Linear or Swing?? Speed of animation? What on mobile?
-    // $('#third-card').animate({'width':'show'} , { duration: 1500, queue: true, easing: "linear" }); //Linear or Swing?? Speed of animation? What on mobile?
 
 });
 
