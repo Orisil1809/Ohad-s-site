22116cb055a461d97eaaa48510f66b54d6c50b31
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-21 16:20:01 +0300

    Some more style changes

--- scripts/home2.js
+++ scripts/home2.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
   });
 
   // $('#home-text').animate({'height':'show'} , { duration: 1000, easing: "swing" });
-  tl.to (".p1", {y: "0%", delay: 0.5, duration: 1, stagger: 0.5});
+  tl.to (".text", {y: "0%", delay: 0.5, duration: 1, stagger: 0.5});
   tl.to("#home-text", {y: "0%", delay: 0.5, duration: 1}, "-=3");
   TweenMax.set(".hand-x", {rotation:-20, transformOrigin:"bottom center", x:-20});
   var tween = TweenMax.to(".hand-x", 1, {x:20, rotation:20, repeat:100, ease:Power3.easeInOut, yoyo:true})
