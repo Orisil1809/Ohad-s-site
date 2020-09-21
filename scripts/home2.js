5ed926922e10e19d25102958d768ef4c6ee07180
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-21 14:36:43 +0300

    Try home2 animation of text

--- scripts/home2.js
+++ scripts/home2.js
@@ -1,128 +1,132 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" }});
+
 //#region slick carousel
 
 $(document).ready(function(){
 
-    $('.slick').slick({
-    lazyLoad: 'ondemand',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots:true,
-    speed: 350,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    loop: true,
-    rtl: true,
-    pauseOnFocus: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 848,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dots: false
-          
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          arrows: false,
-          speed: 250,
-          cssEase: 'linear'
-        }
+  $('.slick').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots:true,
+  speed: 350,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  loop: true,
+  rtl: true,
+  pauseOnFocus: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
       }
-    ]
-    });
-
-    $('#home-text').animate({'height':'show'} , { duration: 1000, easing: "swing" });
-    TweenMax.set(".hand-x", {rotation:-20, transformOrigin:"bottom center", x:-20});
-    var tween = TweenMax.to(".hand-x", 1, {x:20, rotation:20, repeat:100, ease:Power3.easeInOut, yoyo:true})
-    
-    let window_height = window.innerHeight;
-    console.log(window_height);
-    $('#header').height(window_height);
-
+    },
+    {
+      breakpoint: 848,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false
+        
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        speed: 250,
+        cssEase: 'linear'
+      }
+    }
+  ]
   });
-  
-  // window.onload = function(){
 
-  // };
+  // $('#home-text').animate({'height':'show'} , { duration: 1000, easing: "swing" });
+  tl.to (".p1", {y: "0%", delay: 0.5, duration: 1, stagger: 0.5});
+  tl.to("#home-text", {y: "0%", delay: 0.5, duration: 1}, "-=3");
+  TweenMax.set(".hand-x", {rotation:-20, transformOrigin:"bottom center", x:-20});
+  var tween = TweenMax.to(".hand-x", 1, {x:20, rotation:20, repeat:100, ease:Power3.easeInOut, yoyo:true})
+  
+  // let window_height = window.innerHeight;
+  // console.log(window_height);
+  // $('#header').height(window_height);
+
+});
+
+// window.onload = function(){
+
+// };
 
 
-  function resizeHeader(){
-    let window_height = window.innerHeight;
-    $('#header').height(window_height);
-  }
+// function resizeHeader(){
+//   let window_height = window.innerHeight;
+//   $('#header').height(window_height);
+// }
 
-  // window.addEventListener('resize', resizeHeader);
+// window.addEventListener('resize', resizeHeader);
 
-  
-  $(window).scroll( function(){
-      // if(window)
-    /* Check the location of each desired element */
-    $('#our_story').each( function(i){
-  
-        // var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
-        const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
-        const bottom_of_window = $(window).scrollTop() + $(window).height();
-  
-        /* If the object is completely visible in the window, fade it in */
-        if( bottom_of_window > middle_of_object ){          
-            // $(this).animate({'opacity':'1'} ,'slow');
-            $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" }); //Linear or Swing?? Speed of animation? What on mobile?
-  
-  
-        }
-  
-    }); 
-  
-  });
-  
+
+$(window).scroll( function(){
+    // if(window)
+  /* Check the location of each desired element */
+  $('#our_story').each( function(i){
+
+      // var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
+      const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
+      const bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if( bottom_of_window > middle_of_object ){          
+          // $(this).animate({'opacity':'1'} ,'slow');
+          $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" }); //Linear or Swing?? Speed of animation? What on mobile?
+
+
+      }
+
+  }); 
+
+});
+
 
 $('.navbar-toggler').click(function(){
-  $("#main_navbar").toggleClass('colored');
-  $('.colored #navbarCollapse').on("click", ".navbar-nav li #contact" , function(){
-    
-    $('#navbarCollapse').collapse('hide');
-    $('.navbar-toggler').removeClass('opened');
-    $('.navbar-toggler').addClass('collapsed');
-    $('#main_navbar').removeClass('colored');
+$("#main_navbar").toggleClass('colored');
+$('.colored #navbarCollapse').on("click", ".navbar-nav li #contact" , function(){
+  
+  $('#navbarCollapse').collapse('hide');
+  $('.navbar-toggler').removeClass('opened');
+  $('.navbar-toggler').addClass('collapsed');
+  $('#main_navbar').removeClass('colored');
 
-  });
+});
 });
 
 
 $(document).scroll(function () {
-  var $nav = $("#main_navbar");
-  $nav.toggleClass('scrolled', $(this).scrollTop() + 20 > 40);
+var $nav = $("#main_navbar");
+$nav.toggleClass('scrolled', $(this).scrollTop() + 20 > 40);
 });
 
 
 //#region Prevent bodyscroll on collapse scroll
 
 $('#navbarCollapse').on('hide.bs.collapse', function () {
-  $('body').removeClass('noscroll')
+$('body').removeClass('noscroll')
 });
 
 
@@ -133,51 +137,51 @@ $('#navbarCollapse').on('hide.bs.collapse', function () {
 
 $('#navbarCollapse').on('shown.bs.collapse', function () {
 
-  $('body').addClass('noscroll')
+$('body').addClass('noscroll')
 });
 
 
 //#endregion
 
 function delay (URL) {
-  setTimeout( function() { window.location = URL }, 500 );
+setTimeout( function() { window.location = URL }, 500 );
 }
 
 $('#scroll-arrow').click(function(){
 
-  var defaultDuration = 777 // ms
-  var edgeOffset = 125 // px
-  zenscroll.setup(defaultDuration, edgeOffset)
-  zenscroll.to(second)
+var defaultDuration = 777 // ms
+var edgeOffset = 125 // px
+zenscroll.setup(defaultDuration, edgeOffset)
+zenscroll.to(second)
 
 });
 
-  //#region our story animation try
-  // $(document).ready( function(){
+//#region our story animation try
+// $(document).ready( function(){
 
-  //   $('#our_story').animate({'opacity':'1'} ,'slow'); //Linear or Swing?? Speed of animation? What on mobile?
-  
-  // });
-  
-  // $(document).ready( function(){
-  //   $('#our_story').animate({'width':'show'} , { duration: 500, easing: "linear" }); //Linear or Swing?? Speed of animation? What on mobile?
-  
-  // });
-  
-  //Animate #our_Story - either width show or opacity change - on scroll or onload?
-  
-  // window.onload =  function(){
-  //   console.log("PRESSED!!!");
-  //   $('#our_story').each(function(i){
-  //     // $(this).animate({ width: 'show'});
-  //     $(this).animate({'opacity':'1'} ,'slow');
-  
-  //   });
-  // };
-  
-  //#endregion
+//   $('#our_story').animate({'opacity':'1'} ,'slow'); //Linear or Swing?? Speed of animation? What on mobile?
 
-  
+// });
+
+// $(document).ready( function(){
+//   $('#our_story').animate({'width':'show'} , { duration: 500, easing: "linear" }); //Linear or Swing?? Speed of animation? What on mobile?
+
+// });
+
+//Animate #our_Story - either width show or opacity change - on scroll or onload?
+
+// window.onload =  function(){
+//   console.log("PRESSED!!!");
+//   $('#our_story').each(function(i){
+//     // $(this).animate({ width: 'show'});
+//     $(this).animate({'opacity':'1'} ,'slow');
+
+//   });
+// };
+
+//#endregion
+
+
 //   $(window).scroll( function(){
 //   $('#second h2').each( function(i){
 
