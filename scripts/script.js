bb98b526ee04ae9781968c6cf8baedd885c3ade2
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-25 19:46:06 +0300

    Add opacity animation to arrow

--- scripts/script.js
+++ scripts/script.js
@@ -78,6 +78,7 @@ $(document).ready(function(){
   // $('#home-text').animate({'height':'show'} , { duration: 1000, easing: "swing" });
   tl.to (".text", {y: "0%", delay: 0.5, duration: 1, stagger: 0.5});
   tl.to("#home-text", {y: "0%", delay: 0.5, duration: 1}, "-=3");
+  tl.to(".scroll-down", {opacity: 1, duration: 1}, "-=0.5");
   // tl.to("#scroll-arrow", {y: "0%", duration: 1});
 
   TweenMax.set(".hand-x", {rotation:-20, transformOrigin:"bottom center", x:-20});
