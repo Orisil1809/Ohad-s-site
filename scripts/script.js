bf14c41fe73bcd5d37130bf14a15d2c488d4decb
Orisil1809 <33377368+Orisil1809@users.noreply.github.com>
2020-09-27 13:24:19 +0300

    Added info modal to home page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" }});
-
+let firstModal = false;
 //#region slick carousel
 
 $(document).ready(function(){
@@ -120,27 +120,25 @@ $(document).ready(function(){
 
 
 
+// $('#myModal').modal(options)
 
 
 $(window).scroll( function(){
-    // if(window)
-  /* Check the location of each desired element */
-  $('#our_story').each( function(i){
+  if(!firstModal){
+    $('#info-section').each( function(i){
+        
+        const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
+        const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
+        const bottom_of_window = $(window).scrollTop() + $(window).height();
 
-      // var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      const top_of_object = $(this).offset().top; //As soon as it appears, animate it in
-      const middle_of_object = $(this).offset().top + $(this).outerHeight()/3; //As soon as it appears, animate it in
-      const bottom_of_window = $(window).scrollTop() + $(window).height();
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > middle_of_object ){    
+          $('#myModal').modal();
+          firstModal = true;
+        }
 
-      /* If the object is completely visible in the window, fade it in */
-      if( bottom_of_window > middle_of_object ){          
-          // $(this).animate({'opacity':'1'} ,'slow');
-          $(this).animate({'width':'show'} , { duration: 1000, easing: "swing" }); //Linear or Swing?? Speed of animation? What on mobile?
-
-
-      }
-
-  }); 
+    }); 
+  }
 
 });
 
